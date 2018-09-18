dfb403d584d8b4d129af69471f7a4518575f2f27
143sample104 <143sample104@gmail.com>
2018-09-18 18:33:39 +0530

    initial commit

--- app.js
+++ app.js
@@ -75,7 +75,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
 }));
-app.get(‘/’, function (req, res) {
+app.get('/', function (req, res) {
  res.send(JSON.stringify({ Hello: ‘World’}));
 });
 
